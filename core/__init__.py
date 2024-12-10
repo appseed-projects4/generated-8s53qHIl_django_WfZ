29107f7dbb41089dc2ac441dd2e7fa9dae2a75ae
AppSeed Generated Projects <133563814+appseed-projects4@users.noreply.github.com>
2024-12-10 10:51:06 +0200

    initial commit

---
+++ core/__init__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+#__CELERY__
+from .celery import app as celery_app
+
+__all__ = ('celery_app',)
+
+from .celery import app as celery_app
+
+__all__ = ('celery_app',)
+
+
